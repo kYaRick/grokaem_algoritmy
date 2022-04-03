8d36a65a5646b981bb477eec584a247defadeb7c
Ярослав Килюшик <3drpuk@gmail.com>
2022-04-03 20:22:55 +0300

    LOL - project)

---
+++ graph.js
@@ -0,0 +1,5 @@
+"use strict";
+
+document.querySelector('#iframe').addEventListener("load", ev => {
+ // document.getElementsByTagName("iframe")[0].iframe.contentWindow.document.getElementById("Fullscreen").click();
+});
